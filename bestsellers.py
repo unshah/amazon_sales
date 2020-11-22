cb31379259d1817b770793e2ab578a2378ced150
ujjwal shah <ujjwal18a@gmail.com>
2020-11-22 11:41:03 -0500

    Added Best Sellers Script

--- bestsellers.py
+++ bestsellers.py
@@ -1,1 +1,6 @@
 #Hello this scrip will list out all the best sellers on Amazon.com
+
+import beautifulsoup from bs4 as soup
+
+
+url = "https://www.amazon.com/Best-Sellers/zgbs/fashion/ref=zg_bs_nav_0"
