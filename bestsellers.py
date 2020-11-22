c0eefbdd90d9fddacaec5b58cfcdc35ea6816f7d
ujjwal shah <ujjwal18a@gmail.com>
2020-11-22 11:53:07 -0500

    Added Best Sellers Script

--- bestsellers.py
+++ bestsellers.py
@@ -5,8 +5,8 @@ from bs4 import BeautifulSoup as bs
 #Load the webpage
 r = requests.get("https://www.amazon.com/Best-Sellers/zgbs/fashion/ref=zg_bs_nav_0")
 
- #convert to bautiful soup object
- soup = bs(r.content)
+#convert to bautiful soup object
+soup = bs(r.content)
 
  #print HTML
  print(soup)
